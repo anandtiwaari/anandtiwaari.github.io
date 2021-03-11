c5c3909dbdd320b979ea12ad5eedc16abd27e401
Anand <anandt607@gmail.com>
2021-03-11 06:33:57 +0530

    pwa

---
+++ scicalc/service-worker.js
@@ -0,0 +1,33 @@
+let resourcesToCache = ["./", "./img/game.png", "./game.js", "./styles.css"];
+
+self.addEventListener("install", e=>{
+    e.waitUntil(
+        caches.open(sci_calc).then(cache =>{
+            return cache.addAll(resourcesToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", e=>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    );
+});
+
+// Update a service worker
+const cacheWhitelist = ['sci-calc'];
+self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
