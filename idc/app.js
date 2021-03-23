94d0e2f0a3fe0934ae116391410913afde478cf8
Anand <anandt607@gmail.com>
2021-03-24 01:34:34 +0530

    theek hai

--- idc/app.js
+++ idc/app.js
@@ -1,40 +1,61 @@
 //selectors
-let menu=document.querySelector('.menu')
-let dikha=document.querySelector('.dikha')
-let cancel=document.querySelector('.fist5')
-let fist=document.querySelector('.fist')
-let fist1=document.querySelector('.fist1')
-let fist2=document.querySelector('.fist2')
-let fist3=document.querySelector('.fist3')
-let fist4=document.querySelector('.fist4')
+let menu = document.querySelector(".menu");
+let dikha = document.querySelector(".dikha");
+let cancel = document.querySelector(".fist5");
+let fist = document.querySelector(".fist");
+let fist1 = document.querySelector(".fist1");
+let fist2 = document.querySelector(".fist2");
+let fist3 = document.querySelector(".fist3");
+let fist4 = document.querySelector(".fist4");
+//moving image
+let backimg = document.querySelector(".back");
 
 //event listeners
-menu.addEventListener('click',show)
-cancel.addEventListener('click',hide)
+menu.addEventListener("click", show);
+cancel.addEventListener("click", hide);
 
 //functions
 
-function show(){
-    // console.log("kaam chall raha hai")
-    menu.style.display="block";
-    dikha.style.display="block";
-    dikha.style.transition="all 1s";
-    dikha.style.width="200px";
-    fist.style.transition='all 1s'
-    fist1.style.display='block'
-    fist2.style.display='block'
-    fist3.style.display='block'
-    fist4.style.display='block'
-
+function show() {
+  // console.log("kaam chall raha hai")
+  menu.style.display = "block";
+  dikha.style.display = "block";
+  dikha.style.transition = "all 1s";
+  dikha.style.width = "200px";
+  fist.style.transition = "all 1s";
+  fist1.style.display = "block";
+  fist2.style.display = "block";
+  fist3.style.display = "block";
+  fist4.style.display = "block";
 }
-function hide(){
-    // console.log("kaam chall raha hai")
-    menu.style.display="block";
-    dikha.style.display="none";
-    dikha.style.width="0px";
-    fist1.style.display='none'
-    fist2.style.display='none'
-    fist3.style.display='none'
-    fist4.style.display='none'
-
+function hide() {
+  // console.log("kaam chall raha hai")
+  menu.style.display = "block";
+  dikha.style.display = "none";
+  dikha.style.width = "0px";
+  fist1.style.display = "none";
+  fist2.style.display = "none";
+  fist3.style.display = "none";
+  fist4.style.display = "none";
 }
+let i = 0;
+let gam = [
+  "one.jpg",
+  "two.jpg",
+  "three.jpg",
+  "four.jpg",
+  "five.jpg",
+  "six.jpg",
+  "seven.jpg",
+  "eighth.jpg",
+];
+function run(){
+    backimg.src = gam[i];
+    if (i < gam.length - 1) {
+      i++;
+    } else {
+      i = 0;
+    }
+    setTimeout('run()',2000)
+}
+onload=run()
